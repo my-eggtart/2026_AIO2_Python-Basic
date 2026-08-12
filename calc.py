f1aab014882fdc8bd29388a282667823dd9b619a
sooy <ai.with.ysy@google.com>
2026-08-12 09:48:53 +0900

    첫 번째 커밋: 프로젝트 세팅 및 gitignore 추가

---
+++ calc.py
@@ -0,0 +1,8 @@
+# calc 모듈 add() 함수 정의
+def add(a, b) :
+    result = a + b
+    return result
+
+def multiply(a, b) :
+    result = a * b
+    return result
---
+++ my_first_package/main.app.py
@@ -0,0 +1,10 @@
+import magic_calc.basic_ops as myops
+import magic_calc.advanced_ops 
+import magic_calc.advanced_ops
+result = magic_calc.advanced_ops.magic_multiply(7)
+
+
+result1 = magic_calc.advanced_ops.sqrt(10)
+
+print(f"마법의 숫자 7을 곱해서 결과값은{result} 10의 제곱근은 {result1}입니다.")
+
